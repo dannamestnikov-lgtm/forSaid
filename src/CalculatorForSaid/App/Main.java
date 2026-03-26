b6587853badefcff3dc0f071556f58ab1d1c01f7
dannamestnikov-lgtm <dan.namestnikov@yandex.ru>
2026-03-26 09:22:23 +0300

    исправлен конвертер и мейн

--- src/CalculatorForSaid/App/Main.java
+++ src/CalculatorForSaid/App/Main.java
@@ -6,25 +6,16 @@ import CalculatorForSaid.Service.Converter;
 import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args) {
-       Calculator cal = new Calculator();
-       int num1 = 11;
-       int num2 = 4;
-       int result1 = cal.plus(num1, num2);
-       Converter con = new Converter();
-       String a = con.convertNum(num1);
-       String b = con.convertNum(num2);
-       String r1 = con.convertNum(result1);
-       String plus = con.convertZnk("+");
-       String ravno = con.convertZnk("=");
-       System.out.println(a + " " + plus + " " + b + " " + ravno + " " + r1);
-       int num3 = 14;
-       int num4 = 11;
-       int result2 = cal.minus(num3, num4);
-       String c = con.convertNum(num3);
-       String d = con.convertNum(num4);
-       String minus = con.convertZnk("-");
-       String r2 = con.convertNum(result2);
-       System.out.println(c + " " + minus + " " + d + " " + ravno + " " + r2);
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        Converter con = new Converter();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int num1 = scan.nextInt();
+        System.out.println("Выберите операцию (+ - * /)");
+        String operation = scan.next();
+        System.out.println("Введите второе число");
+        int num2 = scan.nextInt();
+        System.out.println(con.printOperationText(cal, con, num1, num2, operation));
     }
 }
--- src/CalculatorForSaid/Service/Converter.java
+++ src/CalculatorForSaid/Service/Converter.java
@@ -1,25 +1,112 @@
 package CalculatorForSaid.Service;
 
 public class Converter {
-    private static final String[] num_text = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать", "двадцать"};
+    private static final String[] units = {
+            "", "один", "два", "три", "четыре", "пять",
+            "шесть", "семь", "восемь", "девять", "десять",
+            "одиннадцать", "двенадцать", "тринадцать", "четырнадцать",
+            "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"
+    };
+
+    private static final String[] tens = {
+            "", "", "двадцать", "тридцать", "сорок", "пятьдесят",
+            "шестьдесят", "семьдесят", "восемьдесят", "девяносто"
+    };
+
+    private static final String[] hundreds = {
+            "", "сто", "двести", "триста", "четыреста", "пятьсот",
+            "шестьсот", "семьсот", "восемьсот", "девятьсот"
+    };
+
+    private static final String[] bigUnits = {
+            "", "тысяча", "миллион"
+    };
+
     public String convertNum(int num){
-        if (num > 0 && num < 20){
-           return num_text[num];
+        if (num == 0){
+            return "ноль";
+        }
+        String text = " ";
+
+        if (num >= 1000000){
+            int millions = num / 1000000;
+            text += convertNum(millions) + " " + getform(millions, "миллион", "миллиона", "миллионов") + " ";
+            num %= 1000000;
+        }
+
+        if (num >= 1000) {
+            int thousands = num / 1000;
+            if (thousands == 1) {
+                text += " одна " + getform(thousands, "тысяча", "тысячи", "тысяч") + " ";
+            } else if (thousands == 2) {
+                text += " две " + getform(thousands, "тысяча", "тысячи", "тысяч") + " ";
+            } else {
+                text += convertNum(thousands) + " " + getform(thousands, "тысяча", "тысячи", "тысяч") + " ";
+            }
+            num %= 1000;
+        }
+
+        int h = num / 100;
+        text += hundreds[h] + " ";
+
+        int rem = num % 100;
+        if (rem < 20){
+            text += units[rem];
         } else {
-            return "неизвестно";
+            int t = rem / 10;
+            int u = rem % 10;
+            text += tens[t] + " " + units[u];
+        }
+        return text.trim();
+    }
+
+    public String convertZnak(String operation){
+        switch (operation) {
+            case "+":
+                return "плюс";
+            case "-":
+                return "минус";
+            case "*":
+                return "умножение";
+            case "/":
+                return "деление";
+            case "=":
+                return "равно";
+            default:
+                return "неизвестная операция";
         }
     }
 
-    public String convertZnk(String operation){
+    public static String printOperationText(Calculator cal, Converter con, int num1, int num2, String operation) {
+        int result = 0;
         switch (operation){
             case "+":
-                return "плюс";
-            case "=":
-                return "равно";
+                result = cal.plus(num1, num2);
+                break;
             case "-":
-                return "минус";
+                result = cal.minus(num1, num2);
+                break;
+            case "*":
+                result = cal.ymnozh(num1, num2);
+                break;
+            case "/":
+                result = cal.delenie(num1, num2);
+                break;
             default:
-                return "неизвестно";
+                System.out.println("неизвестная операция");
+                break;
         }
+        return con.convertNum(num1) + " " + con.convertZnak(operation) + " " + con.convertNum(num2) + " " + con.convertZnak("=") + " " + con.convertNum(result);
+    }
+
+    private String getform(int num, String one, String two, String five){
+        int n = num % 100;
+        if (n >= 11 && n <= 19){
+            return five;
+        }
+        int last = n % 10;
+        if (last == 1) return one;
+        if (last >= 2 && last <= 4) return two;
+        return five;
     }
 }
